e8dac55de5a95f22f3e0ceddf9504dd13dd41c2c
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-05-05 22:24:10 -0300

    cadastro livro finalizado

--- src/main/java/com/github/catapan/cadastrolivro/NovoLivroRequest.java
+++ src/main/java/com/github/catapan/cadastrolivro/NovoLivroRequest.java
@@ -5,15 +5,18 @@ import java.math.BigDecimal;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import com.github.catapan.validator.UniqueValue;
+
 public class NovoLivroRequest {
   
   @NotBlank
   String titulo;
 
-  @NotBlank
+  @NotNull
   BigDecimal preco;
 
-  @NotNull
+	@NotBlank
+	@UniqueValue(entity = Livro.class, field = "isbn")
   String isbn;
 
   public NovoLivroRequest(String titulo, BigDecimal preco, String isbn){
@@ -21,6 +24,11 @@ public class NovoLivroRequest {
     this.preco = preco;
     this.isbn = isbn;
   }
+
+  @Override
+	public String toString() {
+		return "NovoLivroRequest [titulo=" + titulo + ", preco=" + preco + ", isbn=" + isbn + "]";
+	}
 
   public Livro toModel() {
     return new Livro(this.titulo, this.preco, this.isbn);
